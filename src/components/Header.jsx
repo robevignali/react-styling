e8dfa51c71a9a1d8051cb7ad2abe727f1df0819e
robevignali <robevignali@gmail.com>
2024-10-11 15:15:22 +0200

    added style-component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
 import logo from '../assets/logo.png';
+import classes from'./header.module.css';
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p 
+        className={true?`${classes.paragraph}`:``}
+        >
+          A community of artists and art-lovers.
+        </p>
     </header>
   );
 }
